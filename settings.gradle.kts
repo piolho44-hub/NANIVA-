014d83f186d3b4d75a0c698c336dc01a61aa6c9d
piolho44-hub <piolho44@gmail.com>
2026-05-20 10:59:25 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,24 @@
 pluginManagement {
-  repositories {
-    google {
-      content {
-        includeGroupByRegex("com\\.android.*")
-        includeGroupByRegex("com\\.google.*")
-        includeGroupByRegex("androidx.*")
-      }
-    }
-    mavenCentral()
-    gradlePluginPortal()
-  }
-}
 
-plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    google()
-    mavenCentral()
-  }
+
+    repositoriesMode.set(
+        RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    )
+
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
-rootProject.name = "My Application"
+rootProject.name = "GestorDeJoias"
 
 include(":app")
