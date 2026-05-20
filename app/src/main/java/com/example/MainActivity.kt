27887b0b9f0e305d246eee3b733240aa6fb1ccee
piolho44-hub <piolho44@gmail.com>
2026-05-20 11:01:41 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -3,13 +3,9 @@ package com.aistudio.gestordejoias.jrwlpq
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
 
@@ -17,16 +13,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+
             MaterialTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Box(
-                        modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text("Gestor de Joias funcionando")
-                    }
+
+                Surface {
+
+                    Text(
+                        text = "Aplicativo funcionando"
+                    )
                 }
             }
         }
